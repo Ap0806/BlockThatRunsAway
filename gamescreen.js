7b7ae9fcd0351a43e83cf94ab586d142ff355f61
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2020-06-30 23:31:56 +0400

    Changed theme
    Changed the theme, increased difficulty, overall refining

--- gamescreen.js
+++ gamescreen.js
@@ -4,6 +4,14 @@ var outer = document.getElementById("outer");
 var color = 0;
 var bad = document.querySelectorAll(".badBlocks");
 var score = 0;
+var body = document.getElementById("body");
+var info = document.getElementById("info");
+var heading = document.getElementById("header");
+var setting = 60;
+
+// HAVE TO ADD A SETTING ELEMENT TO THIS - ]
+// EASY, MEDIUM, HARD, - ACCORDING, WILL 
+// ADD BLOCKS FASTER OR SLOWER
 
 a.addEventListener('mouseover', e => {
     a.style.backgroundColor = "red";
@@ -13,7 +21,9 @@ a.addEventListener('mouseover', e => {
     score+=10;
     scoreCard.innerHTML = "Score: " + score;
     if(score == 1200){
-        outer.style.backgroundColor = "white";
+        outer.style.backgroundColor = "black";
+        body.style.backgroundColor = "black";
+        outer.style.color = "white";
         outer.style.border = "5px solid black";
         outer.style.textAlign = "center";
         outer.style.fontFamily = "Muli";
@@ -23,11 +33,21 @@ a.addEventListener('mouseover', e => {
         outer.innerHTML = "You won!";
         return;
     }
+
     if(score % 100 == 0 && color <= 239 && score !== 0){
         color = color + 20;
         outer.style.backgroundColor = "rgb("+ color + "," + color + "," + color + ")";
+        body.style.backgroundColor = "rgb("+ color + "," + color + "," + color + ")";
     }    
-    if(score % 60 == 0){
+    else if(score > 1000){
+        outer.style.backgroundColor = "rgb(255,255,255)";
+        body.style.backgroundColor = "rgb(255,255,255)";
+        heading.style.color = "black";
+        info.style.color = "black";
+        scoreCard.style.color = "black";
+    }
+
+    if(score % setting == 0){
         createNewDiv();
     }
 
@@ -36,7 +56,7 @@ a.addEventListener('mouseover', e => {
 
 a.addEventListener('mouseout', e => {
   a.style.backgroundColor = "white";
-  a.style.transitionDuration = "1s";  
+  a.style.transitionDuration = "0.1s";  
 });
 
 function createNewDiv() {
@@ -50,14 +70,16 @@ function createNewDiv() {
     d.style.left = getRndInteger(130,1350) + "px";
     document.getElementById("outer").appendChild(d);
     outer.lastChild.addEventListener('mouseover', e=> {
-        outer.style.backgroundColor = "white";
-        outer.style.border = "5px solid black";
+        outer.style.backgroundColor = "black";
+        body.style.backgroundColor = "black";
+        // outer.style.border = "5px solid black";
         outer.style.textAlign = "center";
         outer.style.fontFamily = "Muli";
         outer.style.fontSize = "5em";
         outer.style.padding = "0";
-        outer.style.transition = "background-color 0.2s, border 0.2s";
-        outer.style.paddingTop = "500px";
+        outer.style.transition = "background-color 0.2s, border 0.2s, color 0.2s";
+        outer.style.paddingTop = "100px";
+        outer.style.color = "white";
         outer.innerHTML = "Sorry, you lost. <br> Final Score : "+ score;
 
     });
