b486c790bf791a9c7679a4f50019807d9ee455d2
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2020-07-15 22:23:04 +0400

    Changed to relative measurements
    Suitable for different window sizes

--- gamescreen.js
+++ gamescreen.js
@@ -12,7 +12,6 @@ var info = document.getElementById("info");
 var heading = document.getElementById("header");
 var timer = document.getElementById("timer");
 var setting = 60;
-let flag = 1;
 let s = 59;
 var timerInterval;
 var playB = document.getElementById("playbutton");
@@ -27,6 +26,11 @@ function playButtonClick(){
   backButton.style.display = "block";
 }
 
+/**
+ * @function startGame() - executed once level is chosen - to begin the game
+ * main set up of elements such as attaching event listeners and beginning timer
+ * is done here.
+ */
 function startGame(levelOfDiff){
     if(levelOfDiff == 'med'){
         setting = 80;
@@ -53,6 +57,7 @@ function startGame(levelOfDiff){
         min = 1;
         sec = 30;
     }
+    scoreCard.innerHTML = "Score: 0<br>Final Target = " + finalTarget;
     forDialogueDrop = 1;
     levelDescr.style.display = "none";
     levelDiv.style.display = "none";
@@ -60,60 +65,64 @@ function startGame(levelOfDiff){
     scoreCard.style.display = "block";
     timer.style.display = "block";
     sideText.style.display = "none";
-    displayTime();    
+    displayTime();  
+    whiteBoxMoving();  
 }  
  
 
-
-whiteBox.addEventListener('mouseover', e => {
-    whiteBox.style.backgroundColor = "red";
-    whiteBox.style.transitionDuration = "0s";
-    whiteBox.style.top = getRndInteger(100,700) + "px";
-    whiteBox.style.left = getRndInteger(130,1250) + "px";
-    score+=10;
-    scoreCard.innerHTML = "Score: " + score;
-    if(score == finalTarget){
-        winningText();
-        return;
-    }
-
-    if(score % 100 == 0 && color <= 239 && score !== 0 && score <= (finalTarget - 200)){
-        color = color + 20;
-        body.style.transition = "background-color 0.5s";
-        backButton.style.transition = "background-color 0.5s";
-        body.style.backgroundColor = "rgb("+ color + "," + color + "," + color + ")";
-        backButton.style.backgroundColor = "rgb("+ color + "," + color + "," + color + ")";
-
-    }    
-    else if(score > (finalTarget - 200)){
-        body.style.transition = "background-color 0.5s";
-        backButton.style.transition = "background-color 0.5s";
-        if(finalTarget == 700){
-            body.style.backgroundColor = "rgb(255,255,255)";
-            backButton.style.backgroundColor = "rgb(255,255,255)";
+/**
+ * @function whiteBoxMoving - to attach eventListeners to the white box
+ */
+function whiteBoxMoving(){
+    whiteBox.addEventListener('mouseover', e => {
+        whiteBox.style.backgroundColor = "red";
+        whiteBox.style.transitionDuration = "0s";
+        whiteBox.style.top = getRndInteger(15,85) + "vh";
+        whiteBox.style.left = getRndInteger(10,82) + "vw";
+        score+=10;
+        scoreCard.innerHTML = "Score: " + score + "<br>Final Target: " + finalTarget;
+        if(score == finalTarget){
+            winningText();
+            return;
         }
-        else{
-            body.style.backgroundColor = "rgb(250,250,250)";
-            backButton.style.backgroundColor = "rgb(250,250,250)";
+    
+        if(score % 100 == 0 && color <= 239 && score !== 0 && score <= (finalTarget - 200)){
+            color = color + 20;
+            body.style.transition = "background-color 0.5s";
+            body.style.backgroundColor = "rgb("+ color + "," + color + "," + color + ")";
+    
+        }    
+        else if(score > (finalTarget - 200)){
+            body.style.transition = "background-color 0.5s";
+            if(finalTarget == 700){
+                body.style.backgroundColor = "rgb(255,255,255)";
+            }
+            else{
+                body.style.backgroundColor = "rgb(250,250,250)";
+            }
+            heading.style.color = "black";
+            info.style.color = "black";
+            scoreCard.style.color = "black";
+            timer.style.color = "black";
         }
-        heading.style.color = "black";
-        info.style.color = "black";
-        scoreCard.style.color = "black";
-        timer.style.color = "black";
-    }
-
-    if(score % setting == 0){
-        createNewDiv();
-    }
-});
-
-
-whiteBox.addEventListener('mouseout', e => {
-  whiteBox.style.backgroundColor = "white";
-  whiteBox.style.transitionDuration = "0.1s";  
-});
+    
+        if(score % setting == 0){
+            createNewDiv();
+        }
+    });
+    
+    
+    whiteBox.addEventListener('mouseout', e => {
+      whiteBox.style.backgroundColor = "white";
+      whiteBox.style.transitionDuration = "0.1s";  
+    });
+}
 
 
+
+/**
+ * @function createNewDiv - function to create new red blocks 
+ */
 function createNewDiv() {
     var d = document.createElement("div");
     d.style.width = "20px";
@@ -121,8 +130,8 @@ function createNewDiv() {
     d.style.backgroundColor = "red";
     d.className = "badBlocks";
     d.style.position = "absolute";
-    d.style.top = getRndInteger(100,700) + "px";
-    d.style.left = getRndInteger(130,1350) + "px";
+    d.style.top = getRndInteger(20,85) + "vh";
+    d.style.left = getRndInteger(10,82) + "vw";
     if(isAnimated == 't'){
         console.log("WE ARE HERE WHY ARE YOU NOT MOVING");
         d.style.animation = "10s infinite upanddown";
@@ -137,6 +146,10 @@ function getRndInteger(min, max) {
  return Math.floor(Math.random() * (max - min + 1))  + min;
 }
 
+/**
+* @function losingText() - to display losing message once red block is toucher/**
+*                         timer runs out. 
+*/
 function losingText(){
     forDialogueDrop = 2;
     outer= document.createElement('p');
@@ -164,8 +177,13 @@ function losingText(){
     outer.style.color = "white";
     outer.innerHTML = "Sorry, you lost. <br> Final Score : "+ score;
     backButton.style.backgroundColor = "rgb(0,0,0)";
+    color = 0;
+    score = 0;
 }
 
+/**
+ * @function winningText() - to display winning text once required score is achieved
+ */
 function winningText(){
     forDialogueDrop = 2;
     outer = document.createElement('p');
@@ -195,6 +213,7 @@ function winningText(){
     outer.innerHTML = "YOU WON!";
     backButton.style.backgroundColor = "rgb(0,0,0)";
     score = 0;
+    color = 0;
 }
 
 
@@ -212,6 +231,7 @@ function restart(){
     if(forDialogueDrop == 0){
         playB.style.display = "block";
         levelDiv.style.display = "none";
+        color = 0;
         return;
     }
     else if(forDialogueDrop == 2){
@@ -219,10 +239,17 @@ function restart(){
         outer.style.display = "none";
         playB.style.display = "block";
         playB.style.transition = "display 1s";
+        sideText.style.display - "block";
+        heading.style.color = "white";
+        info.style.color = "white";
+        forDialogueDrop = 0;
+        color = 0;
     }
     else{
         inner.style.display = "none";
         timer.style.display = "none";
+        heading.style.display = "block";
+        info.style.display = "block";
         scoreCard.style.display = "none";
         clearTimeout(timerInterval);       
         var bad = document.querySelectorAll(".badBlocks");
@@ -232,7 +259,6 @@ function restart(){
         dialogueBox.style.animation = "0.7s 1 dropDown";
         dialogueBox.style.display = "block";
     }
-
 }
 
 /**
@@ -261,10 +287,19 @@ function dialogue(answer){
         backButton.style.display = "none";
         levelDiv.style.display = "none";
         sideText.style.display = "block";
+        heading.style.display = "block";
+        info.style.display = "block";
         dialogueBox.style.display = "none";
         body.style.backgroundColor = "black";
+        finalTarget = 0;
         score = 0;
         forDialogueDrop = 0;
+        heading.style.color = "white";
+        info.style.color = "white";
+        scoreCard.style.color = "white";
+        timer.style.color = "white";
+        sideText.style.color = "white";
+        color = 0;
     }
 }
 
